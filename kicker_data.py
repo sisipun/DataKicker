36b99e56b83b58c84a4f8b516b81dbdb983130ec
Alexey <sisipun@gmail.com>
2018-07-23 21:41:42 +0300

    date add

--- kicker_data.py
+++ kicker_data.py
@@ -12,3 +12,19 @@ def get_kicker_data():
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=17)
     return x_train, x_test, y_train, y_test
+
+
+def get_kicker_data_with_date():
+    df = pd.read_csv('kicker_with_date.csv')
+
+    y = df['red_won']
+    df['year'] = pd.to_datetime(df['played_on']).map(lambda d: int(d.date().year))
+    df['month'] = pd.to_datetime(df['played_on']).map(lambda d: int(d.date().month))
+    df['day'] = pd.to_datetime(df['played_on']).map(lambda d: int(d.date().day))
+    df.drop(['red_won', 'red_team_goals', 'yellow_team_goals', 'played_on'], axis=1, inplace=True)
+    df = pd.get_dummies(df, columns=['red_team_p1_id', 'red_team_p2_id', 'yellow_team_p1_id', 'yellow_team_p2_id'])
+    x = df.values
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+    return x_train, x_test, y_train, y_test
+
--- random_forest.py
+++ random_forest.py
@@ -1,4 +1,4 @@
-from kicker_data import get_kicker_data
+from kicker_data import get_kicker_data, get_kicker_data_with_date
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
@@ -7,14 +7,14 @@ print('Random Forest')
 
 X_train, X_test, y_train, y_test = get_kicker_data()
 
-rf = RandomForestClassifier(7, max_features=32, random_state=17)
+rf = RandomForestClassifier(1000, max_depth=4, max_features=40)
 rf.fit(X_train, y_train)
 forest_predictions = rf.predict(X_test)
 print(accuracy_score(y_test, forest_predictions))
 
-forest_params = {'max_depth': range(1, 21),
-                 'max_features': range(30, 41)}
+forest_params = {'max_depth': range(1, 51),
+                 'max_features': range(1, 51)}
 
-locally_best_forest = GridSearchCV(rf, forest_params, cv=5)
+locally_best_forest = GridSearchCV(rf, forest_params, cv=3)
 locally_best_forest.fit(X_train, y_train)
 print(locally_best_forest.best_params_, locally_best_forest.best_score_)
